ad4e1494ab47db122e71edfb0dc70d0913ab9b0f
HaifaAbdulSathar <149424278+HaifaAbdulSathar@users.noreply.github.com>
2024-12-13 22:46:37 +0400

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,3 +1,12 @@
+# /// script
+# dependencies = [
+#     "pandas",
+#     "numpy",
+#     "seaborn",
+#     "matplotlib",
+#     "requests",
+# ]
+# ///
 import pandas as pd
 import numpy as np
 import seaborn as sns
